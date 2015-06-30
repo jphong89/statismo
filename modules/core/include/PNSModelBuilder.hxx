134e149cbf1dbc8720d1f8ad5fb854c512dcddf6
Jp Hong <jphong@nike.ia.unc.edu>
2015-06-30 15:57:49 -0400

    PNS riemannina log/exp map implemented

--- modules/core/include/PNSModelBuilder.hxx
+++ modules/core/include/PNSModelBuilder.hxx
@@ -224,7 +224,70 @@ namespace statismo {
             }
         }
 
+    template <typename T>
+        typename PNSModelBuilder<T>::MatrixXd
+        PNSModelBuilder<T>::computeRotMat( const VectorXd& vec ) const {
 
+            Eigen::MatrixXd rotMat( vec.size(), vec.size() );
+            roMat.setIdentity();
+
+            Eigen::VectorXd northPole = Eigen::Zero( vec.size() );
+            northPole( vec.size() - 1 ) = 1;
+
+            Eigen::VectorXd vecUnit = vec;
+            vecUnit.normalize();
+
+            double mult = northPole.adjoint() * vecUnit;
+            double alpha = acos( mult );
+
+            if ( abs( mult -1) < 1e-15 ) {
+                return rotMat;
+            }
+            if ( abs( mult +1) < 1e-15 ) {
+                rotMat*= -1;
+                return rotMat;
+            }
+            Eigen::VectorXd auxVec = vecUnit - mult*northPole;
+            aux.normalize();
+            Eigen::MatrixXd auxMat = vecUnit*aux.transpose() - aux*vecUnit.transpose();
+            rotMat += sin(alpha)*auxMat + (cos(alpha) - 1)*(vecUnit*vecUnit.transpose() + auxVec*auxVec.transpose());
+            return rotMat;
+        }
+    template <typename T>
+        typename PNSModelBuilder<T>::MatrixXd 
+        PNSModelBuilder<T>::computeRiemannianExpMap( const MatrixXd& mat ) const {
+            
+            MatrixXd result( mat.rows()+1, mat.cols() );
+            
+            RowVectorXd normVec     = mat.colwise().norm();
+            RowVectorXd normVec2    = (normVec.array() < 1e-16).select(0,normVec);
+            RowVectorXd sineVec     = normVec2.array().sin();
+            RowVectorXd cosVec      = normVec2.array().cos();
+
+            RowVectorXd auxVec      = sineVec.array() / normVec.array();            
+            MatrixXd    auxMat      = auxVec.replicate( mat.rows(), 1 );
+            auxMat                  = (auxMat.array() * mat.array()).matrix();
+            result << auxMat, cosVec;
+            
+            return result;
+
+        }
+    template <typename T>
+        typename PNSModelBuilder<T>::MatrixXd
+        PNSModelBuilder<T>::computeRiemannianLogMap( const MatrixXd& mat ) const {
+            
+            MatrixXd result(mat.rows()-1, mat.cols());
+            
+            RowVectorXd lastRow = mat.row(mat.rows() - 1);
+            RowVectorXd auxV1   = (lastRow.array() * lastRow.array()).matrix();
+                        auxV1   = (1 - auxV1.array()).matrix();
+            RowVectorXd auxV2   = (lastRow.array().acos()).matrix();
+            RowVectorXd auxV3   = auxV1.array() / auxV2.array();
+            RowVectorXd scale   = (auxV1.array() < 1e-64 && auxV2.array() < 1e-64).select(1,auxV3);
+            MatrixXd    auxM1   = scale.replicate( mat.rows() - 1, 1 );
+            MatrixXd    auxM2   = (mat.topRows( mat.rows() - 1 ));
+                        result  = auxM1.array() * auxM2.array();
+
+        }
 } // namespace statismo
-
 #endif
