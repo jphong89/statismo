e37fcbde85e5e08d532572357a4605a2c589adfa
Jp Hong <jphong@nike.ia.unc.edu>
2015-07-01 16:29:48 -0400

    Geodesic mean S1 is being implemented

--- modules/core/include/PNSModelBuilder.hxx
+++ modules/core/include/PNSModelBuilder.hxx
@@ -254,40 +254,49 @@ namespace statismo {
             return rotMat;
         }
     template <typename T>
-        typename PNSModelBuilder<T>::MatrixXd 
+        typename PNSModelBuilder<T>::MatrixXd
         PNSModelBuilder<T>::computeRiemannianExpMap( const MatrixXd& mat ) const {
-            
+
             MatrixXd result( mat.rows()+1, mat.cols() );
-            
+
             RowVectorXd normVec     = mat.colwise().norm();
             RowVectorXd normVec2    = (normVec.array() < 1e-16).select(0,normVec);
             RowVectorXd sineVec     = normVec2.array().sin();
             RowVectorXd cosVec      = normVec2.array().cos();
 
-            RowVectorXd auxVec      = sineVec.array() / normVec.array();            
+            RowVectorXd auxVec      = sineVec.array() / normVec.array();
             MatrixXd    auxMat      = auxVec.replicate( mat.rows(), 1 );
             auxMat                  = (auxMat.array() * mat.array()).matrix();
             result << auxMat, cosVec;
-            
+
             return result;
 
         }
     template <typename T>
         typename PNSModelBuilder<T>::MatrixXd
         PNSModelBuilder<T>::computeRiemannianLogMap( const MatrixXd& mat ) const {
-            
+
             MatrixXd result(mat.rows()-1, mat.cols());
-            
+
             RowVectorXd lastRow = mat.row(mat.rows() - 1);
             RowVectorXd auxV1   = (lastRow.array() * lastRow.array()).matrix();
-                        auxV1   = (1 - auxV1.array()).matrix();
+            auxV1   = (1 - auxV1.array()).matrix();
             RowVectorXd auxV2   = (lastRow.array().acos()).matrix();
             RowVectorXd auxV3   = auxV1.array() / auxV2.array();
-            RowVectorXd scale   = (auxV1.array() < 1e-64 && auxV2.array() < 1e-64).select(1,auxV3);
+            // This line is to check for NaN occurrences.
+            // If any entry in auxV3 is NaN then replace it with 1.
+            RowVectorXd scale   = (auxV1.array() < 1e-64 && auxV2.array() < 1e-64).select(1,auxV3); 
             MatrixXd    auxM1   = scale.replicate( mat.rows() - 1, 1 );
             MatrixXd    auxM2   = (mat.topRows( mat.rows() - 1 ));
-                        result  = auxM1.array() * auxM2.array();
+            result  = auxM1.array() * auxM2.array();
 
         }
+
+    template <typename T>
+        typename PNSModelBuilder<T>::VectorXd
+        PNSModelBuilder<T>::computeGeodesicMeanS1( const MatrixXd& mat ) const {
+
+        }
+
 } // namespace statismo
 #endif
