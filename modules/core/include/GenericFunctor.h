edaa2dff331df963d5430c0dbc833e526b7c28a0
Jp Hong <jphong@nike.ia.unc.edu>
2015-07-09 17:47:55 -0400

    PNS Nonlinear Optimization started to implement

---
+++ modules/core/include/GenericFunctor.h
@@ -0,0 +1,39 @@
+#ifndef __GENERICFUNCTOR_H__
+#define __GENERICFUNCTOR_H__
+#include <iostream>
+#include <unsupported/Eigen/NonLinearOptimization>
+//using namespace Eigen;
+
+template <typename _Scalar, int NX=Eigen::Dynamic, int NY=Eigen::Dynamic>
+class GenericFunctor {
+    //protected:
+    public:
+        typedef _Scalar Scalar;
+        enum{
+            InputsAtCompileTime = NX,
+            ValuesAtCompileTime = NY
+        };
+
+        typedef Eigen::Matrix<Scalar,InputsAtCompileTime,1> InputType;
+        typedef Eigen::Matrix<Scalar,InputsAtCompileTime, ValuesAtCompileTime> ValueType;
+        //        typedef Eigen::Matrix<Scalar,ValuesAtCompileTime,InputsAtCompileTime> JacobianType;
+        typedef Eigen::MatrixXd MatrixXd;
+        typedef Eigen::VectorXd VectorXd;
+
+        int m_inputs, m_values;
+
+        //VectorXd m_y;
+        ValueType m_y;
+        //public:
+        GenericFunctor() : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime) {}
+        GenericFunctor(int inputs, int values, const ValueType& y) : m_inputs(inputs), m_values(values), m_y(y) {}
+
+        int inputs() const { return m_inputs; }
+        int values() const { return m_values; }
+        // you should define that in the subclass :
+        //  void operator() (const InputType& x, ValueType* v, JacobianType* _j=0) const;
+};
+
+
+
+#endif /*__GENERICFUNCTOR_H__*/
---
+++ modules/core/include/sphereResFunctor.h
@@ -0,0 +1,20 @@
+#ifndef __SPHERERESFUNCTOR_H__
+#define __SPHERERESFUNCTOR_H__
+#include <Eigen/Dense>
+#include <cmath>
+#include "GenericFunctor.h"
+
+class sphereResFunctor : public GenericFunctor<double> {
+    public:
+        typedef enum mode_t{
+            ST=0,
+            SC=1,
+            GC=2
+        } mode_t;
+        mode_t m_mode;
+        sphereResFunctor(int sizeX, int sizeY, const ValueType& y, int mode); 
+        int operator() (const VectorXd& x, VectorXd& fvec) const;
+        int df(const VectorXd& x, MatrixXd& fjac) const;
+};
+
+#endif /*__SPHERERESFUNCTOR_H__*/
