3c5806d715c324d644ba3927797f267e1b91bf79
Jp Hong <jphong@nike.ia.unc.edu>
2015-07-15 16:49:55 -0400

    getsubsphere almost done

--- modules/core/include/PNSModelBuilder.h
+++ modules/core/include/PNSModelBuilder.h
@@ -53,7 +53,9 @@ class PNSModelBuilder : public ModelBuilder<T> {
 
 
                 // PNS stuff
-                // Do I want to return pointer instead of a value??
+                // Things to consider: Do I want to return pointer instead of a value??
+                // If I were to return as a pointer, then managing the memory would be a pain
+                // Probably good to take a look at smart pointer stuff and understand how that works
                 MatrixXd computeRotMat( const VectorXd& vec ) const;
                 MatrixXd computeRiemannianExpMap( const MatrixXd& mat ) const;
                 MatrixXd computeRiemannianLogMap( const MatrixXd& mat ) const;
@@ -64,7 +66,7 @@ class PNSModelBuilder : public ModelBuilder<T> {
                 double LMsphereFit( const MatrixXd& data, VectorXd& x, const int itype = 1) const;
                 // internal objective function to be used inside getSubSphere
                 double objFn( const VectorXd& center, const MatrixXd& data, const double r ) const;
-                double computeSubSphere( VectorXd& center, double& error ) const;
+                double computeSubSphere( VectorXd& center, const MatrixXd& data, const int itype = 1, double& error ) const;
                 double getSubSphere( VectorXd& center, const MatrixXd& data, const int itype = 1) const;
         };
 
