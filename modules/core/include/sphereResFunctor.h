0d80a4fd8d71efd113e4830b68205ba7f09d9a05
Jp Hong <jphong@nike.ia.unc.edu>
2015-07-22 13:57:38 -0400

    Why is sphereResFunctor.h not added?

--- modules/core/include/sphereResFunctor.h
+++ modules/core/include/sphereResFunctor.h
@@ -1,15 +1,14 @@
-#ifndef __SPHERERESFUNCTOR_H__
-#define __SPHERERESFUNCTOR_H__
-#include <Eigen/Dense>
-#include <cmath>
+#ifndef __FUNCTOR_H__
+#define __FUNCTOR_H__
 #include "GenericFunctor.h"
 
 class sphereResFunctor : public GenericFunctor<double> {
     public:
         typedef enum mode_t{
-            HT=0,
-            SC=1,
-            GC=2
+            HT,
+            SC,
+            GC,
+            NumEnum
         } mode_t;
         mode_t m_mode;
         sphereResFunctor(int sizeX, int sizeY, const ValueType& y, int mode): GenericFunctor<double>(sizeX, sizeY, y), m_mode( static_cast<mode_t>(mode) ) {}; 
@@ -57,7 +56,4 @@ class sphereResFunctor : public GenericFunctor<double> {
         }
 };
 
-
-#include "sphereResFunctor.hxx"
-
-#endif /*__SPHERERESFUNCTOR_H__*/
+#endif //__FUNCTOR_H__
