fb9d3468b37e45511f44ad0b705a45b1a74ee590
Jp Hong <jphong@nike.ia.unc.edu>
2015-07-22 13:57:38 -0400

    Why is sphereResFunctor.h not added?

--- modules/core/include/sphereResFunctor.h
+++ modules/core/include/sphereResFunctor.h
@@ -1,23 +1,59 @@
-#ifndef __SPHERERESFUNCTOR_H__
-#define __SPHERERESFUNCTOR_H__
-#include <Eigen/Dense>
-#include <cmath>
+#ifndef __FUNCTOR_H__
+#define __FUNCTOR_H__
 #include "GenericFunctor.h"
 
 class sphereResFunctor : public GenericFunctor<double> {
     public:
         typedef enum mode_t{
-            HT=0,
-            SC=1,
-            GC=2
+            HT,
+            SC,
+            GC,
+            NumEnum
         } mode_t;
         mode_t m_mode;
-        sphereResFunctor(int sizeX, int sizeY, const ValueType& y, int mode); 
-        int operator() (const VectorXd& x, VectorXd& fvec) const;
-        int df(const VectorXd& x, MatrixXd& fjac) const;
+        sphereResFunctor(int sizeX, int sizeY, const ValueType& y, int mode): GenericFunctor<double>(sizeX, sizeY, y), m_mode( static_cast<mode_t>(mode) ) {}; 
+
+        int operator() (const VectorXd& x, VectorXd& fvec) const {
+            assert(x.size()==inputs());
+            assert(fvec.size()==values());
+
+            MatrixXd inputMatrix(inputs(), values());
+            MatrixXd auxMatrix(inputs(), values());
+            double radius(0);
+
+            inputMatrix = x.replicate(1, values());
+            auxMatrix   = ( (m_y.array() - inputMatrix.array()).square() ).matrix();
+
+            VectorXd di = (auxMatrix.colwise().sum()).array().sqrt().matrix();
+            if(m_mode != GC) {
+                radius = di.array().mean();
+            }
+            else {
+                radius = M_PI / 2;
+            }
+
+            fvec = ( di.array() - radius ).matrix();
+            return 0;
+        }
+        int df(const VectorXd& x, MatrixXd& fjac) const {
+
+            assert(fjac.rows() == values() && fjac.cols() == inputs());
+            VectorXd rC(values());
+            (*this)(x,rC);
+            double TOL = 1e-10;
+            double dx = 0.25*TOL;
+            VectorXd rd(values());
+            VectorXd xd(inputs());
+
+            for(int i=0;i<inputs();++i) { 
+                xd = x;
+                xd(i) += dx;
+                rd.setZero();
+                (*this)(xd, rd);
+                fjac.col(i) = (rd.array() - rC.array())/dx;
+            }
+            return 0;
+        }
 };
 
-
-#include "sphereResFunctor.hxx"
-
-#endif /*__SPHERERESFUNCTOR_H__*/
+#endif //__FUNCTOR_H__
